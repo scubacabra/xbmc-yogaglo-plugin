66cf5435fa91a237cce1f05b1316566437468bd6
Daniel Mijares <daniel.j.mijares@gmail.com>
2013-11-03 11:40:45 -0500

    utility method for getting the contents (pre javascript-enabled) of the url

--- default.py
+++ default.py
@@ -1,8 +1,11 @@
+import urllib2
 import xbmc
 import xbmcaddon
 import xbmcgui
 import xbmcplugin
- 
+
+import gzip
+import StringIO
 HANDLE = int(sys.argv[1])
 
 pluginName = "plugin.video.yogaglo"
@@ -22,7 +25,36 @@ time = 5000  # ms
 line1 = "This is my first XBMC plugin"
 line2 = "Going to do some YOGA with YogaGlo"
 line3 = "showing this message in python modules"
+def openUrl(url):
+    #create an opener
+    opener = urllib2.build_opener()
+    #Add useragent, sites don't like to interact with scripts
+    opener.addheaders = [
+        ('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:24.0) Gecko/20100101 Firefox/24.0'),
+        ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
+        ('Accept-Language', 'en-gb,en;q=0.5'),
+        ('Accept-Encoding', 'gzip,deflate'),
+        ('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.7'),
+        ('Keep-Alive', '115'),
+        ('Connection', 'keep-alive'),
+        ('Cache-Control', 'max-age=0'),
+        ]
  
+    resp = opener.open(url)
+ 
+    # Compressed (gzip) response...
+    if resp.headers.get( "content-encoding" ) == "gzip":
+            htmlGzippedData = resp.read()
+            stringIO        = StringIO.StringIO( htmlGzippedData )
+            gzipper         = gzip.GzipFile( fileobj = stringIO )
+            htmlData        = gzipper.read()
+    else :
+            htmlData = resp.read()
+ 
+    resp.close()
+     
+    # Return html
+    return htmlData
 
 dialog = xbmcgui.Dialog()
 dialog.ok(__addonname__, line1, line2, line3) 
