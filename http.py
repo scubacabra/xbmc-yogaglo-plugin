624dd4375d48ed10fd2d574c5aa4926f121b660c
Daniel Mijares <daniel.j.mijares@gmail.com>
2013-11-10 18:51:35 -0500

    a bunch of stuff for cookie processing and logging on.  Need to come back and clean up

--- http.py
+++ http.py
@@ -4,11 +4,18 @@ import StringIO
 
 import cookielib
 import mechanize
+import Cookie
 
 import os, re
 
 from BeautifulSoup import BeautifulSoup
 from mechanize import HTTPCookieProcessor
+
+import logging, sys
+
+logger = logging.getLogger("mechanize")
+logger.addHandler(logging.StreamHandler(sys.stdout))
+logger.setLevel(logging.INFO)
 
 def openUrl(url):
     #create an opener
@@ -43,11 +50,39 @@ def openUrl(url):
     return htmlData
 
 def openUrlWithCookie(url, cookie):
-    br = mechanize.Browser()
+    browser = mechanize.Browser()
+    browser.set_debug_http(True)
+    browser.addheaders = [('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:24.0) Gecko/20100101 Firefox/24.0'),
+                      ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
+                      ('Accept-Language', 'en-gb,en;q=0.5'),
+                      ('Accept-Encoding', 'gzip,deflate'),
+                      ('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.7'),
+                      ('Keep-Alive', '115'),
+                      ('DNT', '1')]
     cj = cookielib.LWPCookieJar()
-    br.set_cookiejar(cj)
-    cj.load(cookie , ignore_discard=True, ignore_expires=True)
-    response = br.open(url)
+    browser.set_cookiejar(cj)
+    cj.load(cookie , ignore_discard=True)#, ignore_expires=True)
+    print cj
+    for Cookie in cj:
+        print 'Expires : ', Cookie.expires  ## prints None
+        print 'Discard : ', Cookie.discard  ## prints True , means that the cookie is a
+        ## session cookie
+        print 'Is Expired : ', Cookie.is_expired()  ## prints False
+        print 'Dict :', Cookie.__dict__
+        
+    browser.set_handle_gzip(True)
+    browser.set_handle_redirect(True)
+    browser.set_handle_referer(True)
+    browser.set_handle_robots(False)
+    browser.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time = 1)
+    opener = mechanize.build_opener(HTTPCookieProcessor(cj))
+    mechanize.install_opener(opener)
+    try:
+        response = browser.open(url)
+    except:
+        print "ERERERE" 
+    print response.geturl
+    print response.info()
     html = response.read()
     return html
 
@@ -62,6 +97,13 @@ def login(cookiePath, username, password, signinUrl):
         pass
 
     browser = mechanize.Browser()
+    browser.addheaders = [('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:24.0) Gecko/20100101 Firefox/24.0'),
+                          ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
+                          ('Accept-Language', 'en-gb,en;q=0.5'),
+                          ('Accept-Encoding', 'gzip,deflate'),
+                          ('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.7'),
+                          ('Keep-Alive', '115'),
+                          ('DNT', '1')]
     cookies = cookielib.LWPCookieJar()
     browser.set_cookiejar(cookies)
     browser.set_handle_gzip(True)
@@ -75,13 +117,15 @@ def login(cookiePath, username, password, signinUrl):
     browser.select_form(name="do_User__eventCheckIdentification")
     browser['fields[password]'] = password
     browser['fields[email]'] = username
-    print browser.form
+    #print browser.form
     response2 = browser.submit()
+    print response2.info()
+    print response2.geturl()
     source = response2.read()
     login = check_login(source,username)
     #if login suceeded, save the cookiejar to disk
     if login == True:
-        cookies.save(cookiePath , ignore_discard=True, ignore_expires=True)
+        cookies.save(cookiePath, ignore_discard=True)#, ignore_expires=True)
 
     #return whether we are logged in or not
     return login
@@ -98,3 +142,12 @@ def check_login(source,username):
     else:
         return False
 
+#code to enable running the .py independent of addon for testing
+if __name__ == "__main__":
+#         logged_in = openUrlWithCookie("http://www.yogaglo.com/online-class-429-What-is-Yoga.html", os.getcwd() + '/yogaGLoCookie.lwp')
+#         myusername = 'daniel.j.mijares@gmail.com'
+#         mypassword = 'seminoles35'
+#         login(os.getcwd()+'/ygCookie.lwp',myusername,mypassword, "http://www.yogaglo.com/signin.php")
+        logged_in = openUrlWithCookie("http://www.yogaglo.com/myaccounttoday.php", os.getcwd() + '/ygCookie.lwp')
+#         print logged_in
+        print check_login(logged_in, "Daniel")
