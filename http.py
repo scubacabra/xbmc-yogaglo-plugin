257b9396bca9808442a58f59def9eb7df48fd000
Daniel Mijares <daniel.j.mijares@gmail.com>
2013-11-10 19:08:42 -0500

    cleaning up, setting the mechanize browser in a convenient method and setting all the params there

--- http.py
+++ http.py
@@ -11,12 +11,6 @@ import os, re
 from BeautifulSoup import BeautifulSoup
 from mechanize import HTTPCookieProcessor
 
-import logging, sys
-
-logger = logging.getLogger("mechanize")
-logger.addHandler(logging.StreamHandler(sys.stdout))
-logger.setLevel(logging.INFO)
-
 def openUrl(url):
     #create an opener
     opener = urllib2.build_opener()
@@ -50,38 +44,15 @@ def openUrl(url):
     return htmlData
 
 def openUrlWithCookie(url, cookie):
-    browser = mechanize.Browser()
-    browser.set_debug_http(True)
-    browser.addheaders = [('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:24.0) Gecko/20100101 Firefox/24.0'),
-                      ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
-                      ('Accept-Language', 'en-gb,en;q=0.5'),
-                      ('Accept-Encoding', 'gzip,deflate'),
-                      ('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.7'),
-                      ('Keep-Alive', '115'),
-                      ('DNT', '1')]
+    browser = setupMechanizeBrowser()
     cj = cookielib.LWPCookieJar()
     browser.set_cookiejar(cj)
     cj.load(cookie , ignore_discard=True)#, ignore_expires=True)
     print cj
-    for Cookie in cj:
-        print 'Expires : ', Cookie.expires  ## prints None
-        print 'Discard : ', Cookie.discard  ## prints True , means that the cookie is a
-        ## session cookie
-        print 'Is Expired : ', Cookie.is_expired()  ## prints False
-        print 'Dict :', Cookie.__dict__
-        
-    browser.set_handle_gzip(True)
-    browser.set_handle_redirect(True)
-    browser.set_handle_referer(True)
-    browser.set_handle_robots(False)
-    browser.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time = 1)
     opener = mechanize.build_opener(HTTPCookieProcessor(cj))
     mechanize.install_opener(opener)
-    try:
-        response = browser.open(url)
-    except:
-        print "ERERERE" 
-    print response.geturl
+    response = browser.open(url)
+    print response.geturl()
     print response.info()
     html = response.read()
     return html
@@ -96,21 +67,9 @@ def login(cookiePath, username, password, signinUrl):
     except:
         pass
 
-    browser = mechanize.Browser()
-    browser.addheaders = [('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:24.0) Gecko/20100101 Firefox/24.0'),
-                          ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
-                          ('Accept-Language', 'en-gb,en;q=0.5'),
-                          ('Accept-Encoding', 'gzip,deflate'),
-                          ('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.7'),
-                          ('Keep-Alive', '115'),
-                          ('DNT', '1')]
+    browser = setupMechanizeBrowser()
     cookies = cookielib.LWPCookieJar()
     browser.set_cookiejar(cookies)
-    browser.set_handle_gzip(True)
-    browser.set_handle_redirect(True)
-    browser.set_handle_referer(True)
-    browser.set_handle_robots(False)
-    browser.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time = 1)
     opener = mechanize.build_opener(HTTPCookieProcessor(cookies))
     mechanize.install_opener(opener)
     browser.open(signinUrl)
@@ -142,12 +101,18 @@ def check_login(source):
     else:
         return False
 
-#code to enable running the .py independent of addon for testing
-if __name__ == "__main__":
-#         logged_in = openUrlWithCookie("http://www.yogaglo.com/online-class-429-What-is-Yoga.html", os.getcwd() + '/yogaGLoCookie.lwp')
-#         myusername = 'daniel.j.mijares@gmail.com'
-#         mypassword = 'seminoles35'
-#         login(os.getcwd()+'/ygCookie.lwp',myusername,mypassword, "http://www.yogaglo.com/signin.php")
-        logged_in = openUrlWithCookie("http://www.yogaglo.com/myaccounttoday.php", os.getcwd() + '/ygCookie.lwp')
-#         print logged_in
-        print check_login(logged_in, "Daniel")
+def setupMechanizeBrowser():
+    browser = mechanize.Browser()
+    browser.addheaders = [('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:24.0) Gecko/20100101 Firefox/24.0'),
+                      ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
+                      ('Accept-Language', 'en-gb,en;q=0.5'),
+                      ('Accept-Encoding', 'gzip,deflate'),
+                      ('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.7'),
+                      ('Keep-Alive', '115'),
+                      ('DNT', '1')]
+    browser.set_handle_gzip(True)
+    browser.set_handle_redirect(True)
+    browser.set_handle_referer(True)
+    browser.set_handle_robots(False)
+    browser.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time = 1)
+    return browser
