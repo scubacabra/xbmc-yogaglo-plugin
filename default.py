b9b01ea9d4c0d6c72574121dffb078e0e183a530
Daniel Mijares <daniel.j.mijares@gmail.com>
2013-12-30 12:10:53 -0500

    because all dependent files were moved to resources/lib need to add folder to sys path and then import what I desire.

--- default.py
+++ default.py
@@ -1,6 +1,9 @@
-from yogaglo import YogaGlo
-
+import os
 import sys
+import xbmcaddon
+
+sys.path.append (xbmc.translatePath( os.path.join( xbmcaddon.Addon().getAddonInfo('path'), 'resources', 'lib' )))
+from yogaglo import YogaGlo
 
 yg = YogaGlo(sys.argv[0], int(sys.argv[1]), sys.argv[2])
 yg.processParameters()
