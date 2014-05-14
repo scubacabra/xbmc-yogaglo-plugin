423f09c37bed218914dad6b8c8008f18b6a8daec
Daniel Mijares <daniel.j.mijares@gmail.com>
2014-05-14 13:25:27 -0400

    along with all the other facelifts, yogagglo gets a facelift.  Along with an integration test facelift as well.
    This is not running through everything fully, but rather is running as
    far as the xbmc utility  calls. It might be possible to Mock those calls
    out sometime in the future, to actually test how many times those
    methods were called etc.

--- test/test_yoga_glo_integration.py
+++ test/test_yoga_glo_integration.py
@@ -4,27 +4,53 @@ Created on Jan 2, 2014
 @author: jacobono
 '''
 from nose.tools import assert_equals
-from yogaglo.soup_crawler import SoupCrawler
+from yogaglo.yogaglo import YogaGlo
 
-# Do a full test, from the website, no resources, mocks, and patches
+# Do a full test from the plugin code.  Runs through everything, stubbing out the xbmc stuff -- so nothing to actually test -- but the code is fully run through, gauranteeing no typos in the yogaglo and xbmc_util modules.  soup_crawler module is tested fully, but the yogaglo and xbm_util need to be run through and no way to test them
 class TestYogaGloIntegration(object):
 
-    def setUp(self):
-        self.crawler = SoupCrawler("http://www.yogaglo.com")
+    # def setUp(self):
 
-    def tearDown(self):
-        self.crawler = None
+    # def tearDown(self):
 
-    def test_class_description(self):
-        results = self.crawler.get_yoga_class_description('2956')
-        assert results is not None
+    def test_index(self):
+        yogaglo = YogaGlo("plugin://yogaglo.py", 2, {})
+        results = yogaglo.index()
 
-    def test_noah_maze_parse_teacher_page(self):
-        results = self.crawler.get_classes(
-            'http://www.yogaglo.com/teacher-4-Noah-Maz%C3%A9.html')
-        assert results is not None
+    def test_teacher_category(self):
+        yogaglo = YogaGlo("plugin://yogaglo.py", 2, {'yogaCategory' : "2"})
+        resuls = yogaglo.category_menu()
 
-    def test_kathryn_parse_teacher_page(self):
-        results = self.crawler.get_classes(
-            'http://www.yogaglo.com/teacher-37-Kathryn-Budig.html')
-        assert results is not None
+    def test_style_category(self):
+        yogaglo = YogaGlo("plugin://yogaglo.py", 2, {'yogaCategory' : "3"})
+        resuls = yogaglo.category_menu()
+
+    def test_duration_category(self):
+        yogaglo = YogaGlo("plugin://yogaglo.py", 2, {'yogaCategory' : "4"})
+        resuls = yogaglo.category_menu()
+
+    def test_level_category(self):
+        yogaglo = YogaGlo("plugin://yogaglo.py", 2, {'yogaCategory' : "5"})
+        resuls = yogaglo.category_menu()
+
+    def test_teacher_select(self):
+        yogaglo = YogaGlo("plugin://yogaglo.py", 2, {'yogaCategory' : "2", 'yogagloUrl' : 'http://www.yogaglo.com/teacher-37-Kathryn-Budig.html'})
+        resuls = yogaglo.classes_menu()
+
+    def test_style_select(self):
+        yogaglo = YogaGlo("plugin://yogaglo.py", 2, {'yogaCategory' : "2", 'yogagloUrl' : 'http://www.yogaglo.com/video-Style-26-Ashtanga.html'})
+        resuls = yogaglo.classes_menu()
+
+    def test_level_select(self):
+        yogaglo = YogaGlo("plugin://yogaglo.py", 2, {'yogaCategory' : "2", 'yogagloUrl' : 'http://www.yogaglo.com/video-Level-23-Level-2-3.html'})
+        resuls = yogaglo.classes_menu()
+
+    def test_duration_select(self):
+        yogaglo = YogaGlo("plugin://yogaglo.py", 2, {'yogaCategory' : "2", 'yogagloUrl' : 'http://www.yogaglo.com/video-Duration-75-75-min.html'})
+        resuls = yogaglo.classes_menu()
+
+    def test_yoga_of_the_day_select(self):
+        yogaglo = YogaGlo("plugin://yogaglo.py", 2, {'yogaCategory' : "2", 'yogagloUrl' : 'http://www.yogaglo.com'})
+        resuls = yogaglo.classes_menu()
+
+    # def test_class_play(self):
--- yogaglo/yogaglo.py
+++ yogaglo/yogaglo.py
@@ -1,12 +1,10 @@
-import xbmc
-import xbmcaddon
-import xbmcgui
-
+from xbmc import translatePath, log, LOGDEBUG, LOGNOTICE
+from xbmcaddon import Addon
+from xbmc_util import (addDirs, eod, yoga_class_list_item,
+form_plugin_url, yoga_category_menu_list_item,
+yoga_glo_index_menu_item, yoga_class_play_video)
 import os
-import urllib
-import re
 
-from xbmc_util import addDir, addDirs, eod, listItemResolvedUrl
 from http import openUrl, openUrlWithCookie, login, check_login
 from soup_crawler import SoupCrawler
 
@@ -18,16 +16,21 @@ class YogaGlo:
     yoga_glo_login_url = "http://www.yogaglo.com/eventcontroler.php"
 
     def __init__(self, plugin, handle, plugin_params):
-        print "YogaGlo -- args are %s, %s, %s" % (plugin, handle, plugin_params)
+        log("YogaGlo -- args are %s, %s, %s" %
+            (plugin, handle, plugin_params), LOGDEBUG)
         self.xbmc_plugin = plugin
         self.xbmc_handle = handle
-        self.addon = xbmcaddon.Addon(id=self.plugin_name)
+        self.addon = Addon(id=self.plugin_name)
         self.yoga_glo_plugin_parameters = plugin_params
         self.crawler = SoupCrawler(self.yoga_glo_base_url)
+
         # Dir where plugin settings and cache will be stored
-        self.yoga_glo_addon_profile_path = xbmc.translatePath(
+        self.yoga_glo_addon_profile_path = translatePath(
             self.addon.getAddonInfo('profile')).decode('utf-8')
 
+        # TODO same, no need for decode
+        print translatePath(self.addon.getAddonInfo('profile'))
+        print self.yoga_glo_addon_profile_path
         if not os.path.exists(self.yoga_glo_addon_profile_path):
             os.makedirs(self.yoga_glo_addon_profile_path)
 
@@ -47,152 +50,127 @@ class YogaGlo:
                 print "YogaGlo -- Cookie PHP session seems invalid...logging in again"
                 self.yoga_glo_logged_in = self.yogaGloLogin()
 
-        print "YogaGlo -- DONE WITH INIT"
-
-    def getYogaGloCategory(self, category):
-        if category == "YOTD":
-            return 1
-        elif category == "Teacher":
-            return 2
-        elif category == "Style":
-            return 3
-        elif category == "Level":
-            return 4
-        elif category == "Duration":
-            return 5
-
-
-    def buildClassesMenu(self):
-        #list of items for xbmc to handle (you get a progress bar with this!)
+    # Build plugins index page, with category selection and yoga of the day
+    def index(self):
+        yoga_glo_categories = {'Teacher': 2, 'Style': 3, 'Duration': 5,
+                               'Level': 4, 'Yoga Of The Day': 1 }
         itemList = []
-        classesInformation = self.crawler.get_classes(
-            self.yoga_glo_plugin_parameters['yogagloUrl'])
-        for classInfo in classesInformation:
-            li = xbmcgui.ListItem(label=classInfo['title'],
-                                  label2=classInfo['secondLabel'],
-                                  iconImage=classInfo['coverPicUrl'])
-            li.setInfo('video', {'title': classInfo['title'],
-                                 'plot': classInfo['plot'],
-                                 'Duration': classInfo['duration'],
-                                 'plotoutline': classInfo['secondLabel'],
-                                 'tagline': classInfo['teacher'],
-                                 'genre': "Yoga"})
-            li.setProperty('IsPlayable', 'true')
-            callbackParams = { 'yogaCategory' : 
-                               self.yoga_glo_plugin_parameters['yogaCategory'],
-                               'yogagloUrl' : classInfo['url'], 'play': 1}
-            print classInfo['url']
-            callBackUrl = self.xbmc_plugin + "?" + urllib.urlencode(callbackParams)
-            print callBackUrl
-            itemList.append((callBackUrl, li, False))
+        for category in yoga_glo_categories:
+            log("yogaglo -> building index for category '%s'" % (category), LOGDEBUG)
+            # processing for yoga of the day!
+            if yoga_glo_categories[category] == 1:
+                yotd = self.crawler.get_yoga_of_the_day_title_and_info()
+                title = yotd['title']
+                description = yotd['information']
+                plugin_query = {'yogaCategory' : yoga_glo_categories[category],
+                                'yogagloUrl' : self.yoga_glo_base_url }
+                li = yoga_glo_index_menu_item(title, description)
+                plugin_url = form_plugin_url(self.xbmc_plugin, plugin_query)
+                itemList.append((plugin_url, li, True))
+                continue
 
-        addDirs(self.xbmc_handle, itemList)
-        eod(self.xbmc_handle)
-
-
-    def buildYogaCategoryMenu(self):
-        print "YogaGlo -- Building category menu just selected"
-        itemList = []
-        menu = self.crawler.get_yoga_glo_navigation_information(
-            self.yoga_glo_plugin_parameters['yogaCategory'])
-        print menu
-
-        for item in menu:
-            try:
-                title, ygUrl, imageUrl = item
-            except ValueError:
-                title, ygUrl = item
-                imageUrl = None
-
-            callbackParams = { 'yogaCategory' :
-                               self.yoga_glo_plugin_parameters['yogaCategory'],
-                               'yogagloUrl' : ygUrl }
-            url = self.xbmc_plugin + "?" + urllib.urlencode(callbackParams)
-            if imageUrl is not None:
-                li = xbmcgui.ListItem(label=title, iconImage=imageUrl)
-            else:
-                li = xbmcgui.ListItem(label=title, iconImage="Default.png")
-
-            itemList.append((url, li, True))
-
-        addDirs(self.xbmc_handle, itemList)
-        eod(self.xbmc_handle)
-        print "YogaGlo -- category menu built"
-
-    def buildTopLevelMenu(self):
-        print "YogaGlo -- building initial Menu (teachers, style, level, duration) selection"
-        itemList = []
-        for category in ["Teacher", "Style", "Level", "Duration", "YOTD"]:
             title = category
             description = "Select A" + title
-            callbackParams = { 'yogaCategory' : 
-                               self.getYogaGloCategory(category) }
-            if category == "YOTD": #special title and info for this!
-                info = self.crawler.get_yoga_of_the_day_title_and_info()
-                title = info['title']
-                description = info['information']
-                callbackParams['yogagloUrl'] = self.yoga_glo_base_url
-
-            url = self.xbmc_plugin + "?" + urllib.urlencode(callbackParams)
-            li = xbmcgui.ListItem(label=title, label2=description,
-                                  iconImage="Default.png")
-            itemList.append((url, li, True))
+            plugin_query = {'yogaCategory' : yoga_glo_categories[category]}
+            li = yoga_glo_index_menu_item(title, description)
+            plugin_url = form_plugin_url(self.xbmc_plugin, plugin_query)
+            # list item is a folder, per se
+            itemList.append((plugin_url, li, True))
 
         addDirs(self.xbmc_handle, itemList)
         eod(self.xbmc_handle)
-        print "YogaGlo -- initial Menu built"
 
-    def playYogaGloVideo(self):
-        print urllib.unquote(self.yoga_glo_addon_profile_path)
-        vidPage = self.yoga_glo_plugin_parameters['yogagloUrl']
+    # downselect an upper category to a further filtered category, need to
+    # select one level down from here to see videos
+    def category_menu(self):
+        log("yogaglo -> selecting category %s" %
+            (self.yoga_glo_plugin_parameters['yogaCategory']), LOGDEBUG)
+        itemList = []
+        yoga_category = self.yoga_glo_plugin_parameters['yogaCategory']
+        menu = self.crawler.get_yoga_glo_navigation_information(yoga_category)
+        for item in menu:
+            url = item['url']
+            # using get() passes None instead of an error
+            li = yoga_category_menu_list_item(item['title'], url,
+                                              item.get('image_url'))
+            plugin_query = { 'yogaCategory' : yoga_category,
+                             'yogagloUrl' : url }
+            plugin_url = form_plugin_url(self.xbmc_plugin, plugin_query)
+            # list item is a folder, per se
+            itemList.append((plugin_url, li, True))
+
+        addDirs(self.xbmc_handle, itemList)
+        eod(self.xbmc_handle)
+
+    # list the items for the classes in a certain category, selecting plays the
+    # video associated with the item
+    def classes_menu(self):
+        log("yogaglo -> getting classes for category '%s' at url '%s'" %
+            (self.yoga_glo_plugin_parameters['yogaCategory'],
+             self.yoga_glo_plugin_parameters['yogagloUrl']), LOGDEBUG)
+        #list of items for xbmc to handle (you get a progress bar with this!)
+        itemList = []
+        yoga_category = self.yoga_glo_plugin_parameters['yogaCategory']
+        classesInformation = self.crawler.get_classes(
+            self.yoga_glo_plugin_parameters['yogagloUrl'])
+
+        for classInformation in classesInformation:
+            class_url = classInformation['url']
+            li = yoga_class_list_item(classInformation)
+            plugin_query = { 'yogaCategory' : yoga_category,
+                             'yogagloUrl' : class_url, 'play': 1}
+            plugin_url = form_plugin_url(self.xbmc_plugin, plugin_query)
+            # list item is not a folder
+            itemList.append((plugin_url, li, False))
+
+        addDirs(self.xbmc_handle, itemList)
+        eod(self.xbmc_handle)
+
+    # selected a class item, stream the video for this class
+    # preview of 5 minutes (usually), if not logged in to site
+    # otherwise, full length, HD video streamed
+    def play_class_video(self):
+        log("yogaglo -> play class at url '%s'" %
+            (self.yoga_glo_plugin_parameters['yogagloUrl']), LOGDEBUG)
+        yogaglo_url = self.yoga_glo_plugin_parameters['yogagloUrl']
+
         #logged in, get full video at highest resolution
         if self.yoga_glo_logged_in:
-            html = openUrlWithCookie(vidPage, self.yoga_glo_cookie_path)
-            print re.compile(".*url: '([^']*)'").findall(html)
-            swfUrl = re.compile(".*url: '([^']*)'").findall(html)[0]
-            print re.compile('url: "([^"]*)"').findall(html)
-            playpath = urllib.unquote(re.compile(
-                'url: "([^"]*)"').findall(html)[0])
-            print re.compile(
-                "netConnectionUrl:\s+'([^']*)'").findall(html)[0]
-            rtmpUrl = re.compile(
-                "netConnectionUrl:\s+'([^']*)'").findall(html)[0]
-        else: #not logged in, get preview clip instead
-            html = openUrl(self.yoga_glo_base_url + vidPage)
-            playpath = urllib.unquote(
-                re.compile("url: '(mp4[^']*)'").findall(html)[0])
-            swfUrl = re.compile("url:\s+'([^mp4]+[^']*)'").findall(html)[0]
-            rtmpUrl = re.compile(
-                "netConnectionUrl:\s+'([^']*)'").findall(html)[0]
-
-        print (playpath, swfUrl, rtmpUrl)
-        liz = xbmcgui.ListItem(label="DANIEL", path=rtmpUrl + "/" + playpath)
-        liz.setProperty('PlayPath', playpath);
-        liz.setProperty('SWFPlayer', swfUrl);
-        listItemResolvedUrl(self.xbmc_handle, liz)
+            video_info = self.crawler.get_yogaglo_video_information(
+                yogaglo_url, self.yoga_glo_cookie_path
+            )
+            yoga_class_play_video(video_info['rtmp_url'],
+                                  video_info['play_path'],
+                                  video_info['swf_url'], self.xbmc_handle)
+            return
+            
+        #not logged in, 5-minute preview clip
+        video_info = self.crawler.get_yogaglo_preview_video_information(
+            yogaglo_url)
+        yoga_class_play_video(video_info['rtmp_url'],
+                              video_info['play_path'],
+                              video_info['swf_url'], self.xbmc_handle)
+        return
 
     def processParameters(self):
-        print "YogaGlo parameters: '%s'", self.yoga_glo_plugin_parameters
-
         if 'play' in self.yoga_glo_plugin_parameters:
-            self.playYogaGloVideo()
+            self.play_class_video()
             return
 
         if 'yogagloUrl' in self.yoga_glo_plugin_parameters:
-            self.buildClassesMenu()
+            self.classes_menu()
             return
 
         if 'yogaCategory' in self.yoga_glo_plugin_parameters:
-            self.buildYogaCategoryMenu()
+            self.category_menu()
             return
 
-        print "Building the Top Menu"
-        self.buildTopLevelMenu()
+        self.index()
 
     def yogaGloLogin(self):
         username = self.addon.getSetting('username')
         password = self.addon.getSetting('password')
-        if username != "" and password != "":
+        if username and password:
             print "YogaGlo -- found credentials for username and password, attempting to logon"
             loggedOn = login(self.yoga_glo_cookie_path, username, password,
                              self.yoga_glo_sign_in_url)
