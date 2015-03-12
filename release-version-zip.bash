4348a4fd25328299f9f555e00ec280bc70dc686a
Daniel Mijares <daniel.j.mijares@gmail.com>
2015-03-12 16:46:34 -0400

    don't need to move up a directory first anymore

--- release-version-zip.bash
+++ release-version-zip.bash
@@ -4,6 +4,4 @@ version="0.6"
 zipName="plugin.video.yogaglo-$version.zip"
 xbmc_plugin_name="plugin.video.yogaglo"
 
-cd ../
-
 zip -r $zipName $xbmc_plugin_name -x=*.git* -x=*.pyc -x=*.settings* -x=*test* -x=*.org* -x=*.bash* -x=*.project* -x=*.pydevproject* -x=*nose2.cfg*
